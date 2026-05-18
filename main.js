410a7b82a7f5f8150f27c8123c0b662c0a0272c2
Kamronbek0000 <kamron3277@gmail.com>
2026-05-18 19:38:40 +0500

    main.js

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
-let demoBtn = document.getElementById("demoBtn");
+function showAlert(){
+    alert("Batafsil ma'lumot tez orada qo‘shiladi!");
+}
 
-demoBtn.addEventListener("click", () => {
-  alert("Demo uchun Telegram: @ERMAMBATOV");
+let darkBtn = document.getElementById("darkModeBtn");
+
+darkBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+
+let searchInput = document.getElementById("searchInput");
+let newsList = document.querySelectorAll("#newsList li");
+
+searchInput.addEventListener("keyup", () => {
+
+    let value = searchInput.value.toLowerCase();
+
+    newsList.forEach(item => {
+
+        if(item.textContent.toLowerCase().includes(value)){
+            item.style.display = "block";
+        }else{
+            item.style.display = "none";
+        }
+
+    });
+
+});
+
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", (e)=>{
+
+    e.preventDefault();
+
+    alert("Xabaringiz yuborildi!");
+
+    contactForm.reset();
+
 });
